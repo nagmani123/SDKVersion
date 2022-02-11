502451d4677882587d11725e8d6e872703eb7dee
namani12 <nagmani.kumar@hastree.com>
2022-02-11 18:16:01 +0530

    Intial commit

---
+++ Example/Pods/Target Support Files/Pods-SDKVersion_Example/Pods-SDKVersion_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDKVersion_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SDKVersion_Example
+@end
---
+++ Example/Pods/Target Support Files/SDKVersion/SDKVersion-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SDKVersion : NSObject
+@end
+@implementation PodsDummy_SDKVersion
+@end
